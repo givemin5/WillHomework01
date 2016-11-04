91bb579aeec0709597bd4e82d8d8e454fb249dda
min.chan <min.chan@hstar.com.tw>
2016-11-04 08:57:26 +0800

    * 修改所有的「刪除」邏輯，所有資料都不能真正被刪除，資料庫中也要新增「是否已刪除」欄位 (欄位要設定 bit 型態)

--- WillHomework01/Models/客戶資料Repository.cs
+++ WillHomework01/Models/客戶資料Repository.cs
@@ -6,11 +6,14 @@ namespace WillHomework01.Models
 {   
 	public  class 客戶資料Repository : EFRepository<客戶資料>, I客戶資料Repository
 	{
-
+		public override IQueryable<客戶資料> All()
+		{
+			return base.All().Where(x => x.是否已刪除 != true);
+		}
 	}
 
 	public  interface I客戶資料Repository : IRepository<客戶資料>
 	{
-
+		
 	}
 }
--- WillHomework01/Models/客戶銀行資訊Repository.cs
+++ WillHomework01/Models/客戶銀行資訊Repository.cs
@@ -6,7 +6,10 @@ namespace WillHomework01.Models
 {   
 	public  class 客戶銀行資訊Repository : EFRepository<客戶銀行資訊>, I客戶銀行資訊Repository
 	{
-
+		public override IQueryable<客戶銀行資訊> All()
+		{
+			return base.All().Where(x=>x.是否已刪除!=true);
+		}
 	}
 
 	public  interface I客戶銀行資訊Repository : IRepository<客戶銀行資訊>
